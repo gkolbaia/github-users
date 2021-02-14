e4508403df3d9c0402c843388edfb2263f8078a9
gkolbaia <gkolbaia3@gmail.com>
2021-02-14 22:21:46 +0400

    s

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -8,23 +8,34 @@ import { map } from 'rxjs/operators';
 export class UsersService {
   constructor(private _httpClient: HttpClient) {}
   getUsers() {
-    return this._httpClient.get('https://api.github.com/users');
+    return this._httpClient.get('https://api.github.com/users', {
+      headers: { Authorization: '0b550f5ef37ef0c80bdb8d13b02a0cdd25139814' },
+    });
   }
   getUser(userName: string) {
-    return this._httpClient.get(`https://api.github.com/users/${userName}`);
+    return this._httpClient.get(`https://api.github.com/users/${userName}`, {
+      headers: { Authorization: '0b550f5ef37ef0c80bdb8d13b02a0cdd25139814' },
+    });
   }
   getUserRepositories(userName: string) {
     return this._httpClient.get(
-      `https://api.github.com/users/${userName}/repos?per_page=3`
+      `https://api.github.com/users/${userName}/repos?per_page=3`,
+      {
+        headers: { Authorization: '0b550f5ef37ef0c80bdb8d13b02a0cdd25139814' },
+      }
     );
   }
 
   searchUser(searchQuery: string) {
     return this._httpClient
-      .get(`https://api.github.com/search/users?q=${searchQuery}`)
+      .get(`https://api.github.com/search/users?q=${searchQuery}`, {
+        headers: { Authorization: '0b550f5ef37ef0c80bdb8d13b02a0cdd25139814' },
+      })
       .pipe(map((res: any) => res.items));
   }
   getOrganisations(login: string) {
-    return this._httpClient.get(`https://api.github.com/users/${login}/orgs`);
+    return this._httpClient.get(`https://api.github.com/users/${login}/orgs`, {
+      headers: { Authorization: '0b550f5ef37ef0c80bdb8d13b02a0cdd25139814' },
+    });
   }
 }
